b7528a71776168ee9c64d1473e85796abafccd35
mark <markx931@gmail.com>
2023-11-12 20:42:54 +0100

    Create SettingsApi.cs

---
+++ PaperlessServer/Paperless.rest/Controllers/SettingsApi.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Paperless.rest.Attributes;
+using System;
+
+namespace Paperless.rest.Controllers
+{
+    [ApiController]
+    public class SettingsApi : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <response code="200">Success</response>
+        [HttpGet]
+        [Route("/api/ui_settings/")]
+        [Consumes("application/json", "text/json", "application/*+json")]
+        [ValidateModelState]
+        public virtual IActionResult GetUISettings()
+        {
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200);
+
+            throw new NotImplementedException();
+        }
+    }
+}
