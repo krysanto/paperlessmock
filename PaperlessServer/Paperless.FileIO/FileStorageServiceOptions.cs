b5a73211aa03ac05e5e53b2b38a3e2bb0b89135f
Michael Csida <krysanto@gmail.com>
2024-01-08 22:12:58 +0100

    remove fileio

--- PaperlessServer/Paperless.FileIO/FileStorageServiceOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace Paperless.FileIO
-{
-    public class FileStorageServiceOptions
-    {
-        public const string FileStorage = "FileStorage";
-
-        public string Path { get; set; } = string.Empty;
-    }
-}
